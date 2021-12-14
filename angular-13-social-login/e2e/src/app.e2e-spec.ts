99ae786d6d49bf893dcff5043f8ab1ee3cda36a9
Chinna <inbox4chinna@gmail.com>
2021-12-14 21:37:30 +0530

    Upgraded Angular 12 to 13 and Spring 2.4.0 to 2.6.1

--- angular-13-social-login/e2e/src/app.e2e-spec.ts
+++ angular-13-social-login/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Angular10SocialLogin app is running!');
+    expect(page.getTitleText()).toEqual('Angular13SocialLogin app is running!');
   });
 
   afterEach(async () => {
